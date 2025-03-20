03fc2d8924e8be319e8ac52155244630084cf0ab
Harsh Mishra <hmisraji07@gmail.com>
2025-03-20 16:46:16 +0530

    Revert Duplicate Payments Fix for the sake of Telepresence Intercept Demo
    Signed-off-by: Harsh Mishra <hmisraji07@gmail.com>

--- src/app.py
+++ src/app.py
@@ -15,21 +15,6 @@ payments: Dict[str, Payment] = {}
 ORDER_SERVICE_URL = os.getenv("ORDER_SERVICE_URL", "http://orders-svc.api")
 
 
-def check_if_order_already_paid_for(order_id: str):
-    response = requests.get(f"{ORDER_SERVICE_URL}/orders/{order_id}")
-    if not response.status_code == 200:
-        raise HTTPException(
-            status_code=response.status_code, detail=response.json()
-        )
-    order_info = response.json()
-    order_status = order_info.get("status", None)
-    if not order_status:
-        raise HTTPException(status_code=500, detail="Internal Server Error")
-    print(order_info)
-    if order_status == "paid":
-        raise HTTPException(status_code=403, detail="Already Paid")
-
-
 @app.get("/healthz", status_code=201)
 async def health():
     return {"message": "Hi Mom!"}
@@ -42,8 +27,6 @@ def process_payment(payment: Payment):
 
     # After payment, update the order status in Orders Service
     update_order_url = f"{ORDER_SERVICE_URL}/orders/{payment.order_id}/status"
-    # check if order is already paid
-    check_if_order_already_paid_for(payment.order_id)
     response = requests.patch(update_order_url, params={"status": "paid"})
     if not response.status_code == 200:
         raise HTTPException(status_code=500, detail="Internal Server Error")
